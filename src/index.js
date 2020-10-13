dfec4548e8a008773f6f62c45036830054c4570c
VadimND <webzorka24@gmail.com>
2020-10-13 22:24:49 +0300

    feat: add function for array sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix) {  
+  
+  if (matrix == null || matrix.length == 0 || arguments.length === 0) {
+    return [];
+  } 
+
+  let numArr = matrix.length;
+  let newArr = [];
+  
+  for (let i = 1; i < numArr; i++) {
+    if (!(i % 2 === 0)) {
+      matrix[i].sort(function(a, b) { return b - a; });
+    }
+    newArr.push(matrix[i])
+  }   
+  newArr.unshift(matrix[0]);
+  let res = [];
+  for (let j = 0; j < newArr.length; j++) {
+    for(let g = 0; g < newArr[j].length; g++) {
+      res.push(newArr[j][g]);
+    }
+  }  
+  
+    return res;
+   
 }
